aa9b086feac6548430cc6f83bc09a85f13ca1c9a
Swapna <77243167+swapna2021@users.noreply.github.com>
2024-07-19 14:25:13 +0530

    Delete day1Repo directory

--- day1Repo/ClassA.java
+++
@@ -1,7 +0,0 @@
-package com.graymatter;
-
-public class ClassA {
-	
-	protected String name;
-
-}
--- day1Repo/ClassB.java
+++
@@ -1,9 +0,0 @@
-package com.graymatter;
-
-public class ClassB extends ClassA{
-	
-	public void printDetails() {
-		name="swapna";
-	}
-
-}
